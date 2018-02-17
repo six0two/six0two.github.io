3a383f9c7dd49b602fedfef564a884b39933afc5
six0two <eyescreem.022@gmail.com>
2018-02-17 16:45:16 +0100

    podpinanie funkcji

---
+++ warsztaty/js-ustaw-tlo/main.js
@@ -0,0 +1,6 @@
+function ustawTlo() {
+    var allParagraps = document.getElementsByTagName("p");
+    
+    allParagraps[0].style.backgroundColor = "red";
+    allParagraps[1].style.backgroundColor = "blue";
+}

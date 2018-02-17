c4a692301e5a78ea377b4260d53a649ff04bbeff
six0two <eyescreem.022@gmail.com>
2018-02-17 17:01:08 +0100

    dokończyć wyświetlanie w konsoli

--- warsztaty/js-ustaw-tlo/main.js
+++ warsztaty/js-ustaw-tlo/main.js
@@ -4,3 +4,10 @@ function ustawTlo() {
     allParagraps[0].style.backgroundColor = "red";
     allParagraps[1].style.backgroundColor = "blue";
 }
+
+var formularz = document.getElementsByTagName("form");
+
+document.getElementById( "formularz" ).addEventListener( 'submit', function(e){
+e.preventDefault();
+console.log();
+} );

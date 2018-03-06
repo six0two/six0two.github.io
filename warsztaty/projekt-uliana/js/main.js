be41da0b730b68f625aba02815448985418776e6
six0two <eyescreem.022@gmail.com>
2018-03-06 12:06:22 +0100

    projekt uliany

--- warsztaty/projekt-uliana/js/main.js
+++ warsztaty/projekt-uliana/js/main.js
@@ -1,1 +1,22 @@
 'use strict';
+/* Efekt Smooth Scroll */
+
+$('a[href*="#"]:not([href="#"])').click(function(){
+    console.log('test');
+    $('html, body').animate( { scrollTop: $(this.hash).offset().top - 74 }, 500 );
+});
+
+/* Scroll menu color change */
+
+if($(window).scrollTop() >= 50){
+    $('#main-nav').addClass('scroll');
+}
+
+$(window).scroll(function(){
+    if($(window).scrollTop() >= 50 ) {
+        $('#main-nav').addClass('scroll');
+        $('#main-nav').css({'transition':'all 0.3s'})
+    } else {
+        $('#main-nav').removeClass('scroll');
+    }
+});
